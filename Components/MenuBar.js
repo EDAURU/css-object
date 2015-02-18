53dd820610f9463949bf11e9e78349df2833c15b
Kike Marquez <kike_marquez8@hotmail.com>
2015-02-18 18:07:50 -0430

    added Properties and Events to PopMenu

--- Components/MenuBar.js
+++ Components/MenuBar.js
@@ -1,7 +1,14 @@
 var MenuBar = function () {
 
     var MenuBarProto = Object.create(HTMLDivElement.prototype);
-    
+
+    Object.defineProperties(MenuBarProto,{
+       "tableBar":{
+           get: function(){
+               return this.shadowRoot.getElementById("tableBar");
+           }
+       }
+    });
     /*var getNumber = function (element) {
             return (element.replace("px", "") * 1);
     };*/
@@ -10,46 +17,54 @@ var MenuBar = function () {
         if(typeof color === 'string') this.shadowRoot.getElementsByTagName('table')[0].style.backgroundColor = color;
     };
     MenuBarProto.setSpacing = function(right,top,bottom,left){
-        var table= this.shadowRoot.getElementsByTagName('table')[0];
-        for(var i = 0; i<table.rows[0].cells.length-1;i++) {
+        for(var i = 0; i<this.tableBar.rows[0].cells.length-1;i++) {
                 right = typeof right === 'string'? parseInt(right):right;
                 top = typeof top === 'string'? parseInt(top):top;
                 bottom = typeof bottom === 'string'? parseInt(bottom):bottom;
                 left = typeof left === 'string'? parseInt(left):left;
-                table.rows[0].cells[i].style.paddingRight = right;
-                table.rows[0].cells[i].style.paddingLeft = left;
-                table.rows[0].cells[i].style.paddingBottom = bottom;
-                table.rows[0].cells[i].style.paddingTop = top;
+                this.tableBar.rows[0].cells[i].style.paddingRight = right;
+                this.tableBar.rows[0].cells[i].style.paddingLeft = left;
+                this.tableBar.rows[0].cells[i].style.paddingBottom = bottom;
+                this.tableBar.rows[0].cells[i].style.paddingTop = top;
         }
     };
     MenuBarProto.appendTop = function(){
-        this.shadowRoot.getElementsByTagName('table')[0].style.top=0;
-        this.shadowRoot.getElementsByTagName('table')[0].style.left=0;
-        this.shadowRoot.getElementsByTagName('table')[0].style.paddingRight='100%';
+        this.tableBar.style.top=0;
+        this.tableBar.style.left=0;
+        this.tableBar.style.paddingRight='100%';
 
     }
     MenuBarProto.addItem = function (name) {
-        var cell = this.shadowRoot.getElementsByTagName("table")[0].rows[0].insertCell();
+        var cell = this.tableBar.rows[0].insertCell();
         cell.innerHTML = name;
+
+        cell.onmouseover = function (e) {
+            cell.style.cursor = "default";
+            cell.style.backgroundColor = "darkgray";
+        };
+
+        cell.onmouseout = function (e) {
+            cell.style.backgroundColor = "initial";
+        };
     };
     MenuBarProto.setFontFamily = function(font){
-        this.shadowRoot.getElementsByTagName('table')[0].style.fontFamily = font;
+        this.tableBar.style.fontFamily = font;
     };
 
-    MenuBarProto.addSubMenuToItem = function (idName) {
+/*    MenuBarProto.addSubMenuToItem = function (idName) {
         var subMenu = document.createElement("div");
         subMenu.style.backgroundColor = "rgba(247,247,247,0.7)";
         subMenu.style.position = "absolute";
         subMenu.style.display = "none";
         var self = this;
         for (var i = 0; i < this.shadowRoot.getElementById("tableBar").rows[0].cells.length; i++) {
-            if (this.shadowRoot.getElementById("tableBar").rows[0].cells[i].innerHTML == idName) {
-                this.shadowRoot.getElementById("tableBar").rows[0].cells[i].appendChild(subMenu);
-                this.shadowRoot.getElementById("tableBar").rows[0].cells[i].onmouseover = function (e) {
+            if (this.tableBar.rows[0].cells[i].innerHTML == idName) {
+                this.tableBar.rows[0].cells[i].appendChild(subMenu);
+                this.tableBar.rows[0].cells[i].onmouseover = function (e) {
                     if (e.target.innerHTML == idName)
                         e.target.style.backgroundColor = "darkgrey";
-                    for (var i = 0; i < self.shadowRoot.getElementById("tableBar").rows[0].cells.length; i++) {
-                        self.shadowRoot.getElementById("tableBar").rows[0].cells[i].style.cursor = "default";
+                    for (var i = 0; i <this.tableBar.rows[0].cells.length; i++) {
+                        this.tableBar.rows[0].cells[i].style.cursor = "default";
                     }
                     if (subMenu.style.display == "none")
                         subMenu.style.display = "block";
@@ -71,10 +86,10 @@ var MenuBar = function () {
                 }
             };
         };
-    };
+    };*/
     MenuBarProto.addSubMenuToBar = function (target,items){
         var a = new PopMenu();
-        var table = this.shadowRoot.getElementById('tableBar');
+        var table = this.tableBar;
         a.top = table.style.height;
         var cell= findCell(target,table);
         a.addItems(items);
@@ -84,20 +99,25 @@ var MenuBar = function () {
         a.setFontSize('13px');
         a.setFontSizeHotKey('10px');
         table.rows[0].cells[cell].onclick = function(){
-            a.hidden? a.show(): a.hide();
-        }
+            a.hidden ? a.show() : a.hide();
+        };
         a.onmouseleave = function(){
             a.hide();
-        }
-        a.MouseOverEvent('rgba(249,120,40,0.3)')
-
-    }
+        };
+        a.MouseOverEvent('rgba(249,120,40,0.3)');
+        //console.log(this.tableBar.rows[0].cells[0].getElementsByTagName('div')[0].shadowRoot.getElementsByTagName('div')[0].getEle);
+        //console.log(this.tableBar.rows[0].cells[0].getElementsByTagName('div')[0].shadowRoot.getElementById('PopMenuBar'));
+    };
+    MenuBarProto.addSubMenuToSubMenu = function(targets,items){
+        //var sb= new PopMenu();
+        //console.log(this.tableBar.rows[0].cells[findCell(bartarget)].getElementsByTagName('div')[0].shadowRoot.getElementById('PopMenuBar')[0]);
+    };
 
     function findCell(cell,table){
         for (var i = 0; i < table.rows[0].cells.length; i++)
             if (table.rows[0].cells[i].innerHTML == cell) return i;
     }
-    MenuBarProto.addItemToSubMenu = function (idName, itemName, imgCell) {
+    /*MenuBarProto.addItemToSubMenu = function (idName, itemName, imgCell) {
         var self = this;
         var tableContainer = document.createElement("div");
         var itemTable = document.createElement("table");
@@ -139,7 +159,7 @@ var MenuBar = function () {
             };
         
         }
-    };
+    };*/
     //********************* funcion CallBack **********************//
     
     MenuBarProto.createdCallback = function () {
@@ -164,43 +184,61 @@ var PopMenu = function(){
 
     Object.defineProperties(PopMenuProto,
         {
-            "top":{
-            set: function(newVal){
-                this.shadowRoot.getElementsByTagName('div')[0].style.top=newVal;
+            "top":  {
+                set: function(newVal){
+                    this.shadowRoot.getElementsByTagName('div')[0].style.top=newVal;
+                     },
+                get: function(){
+                    return this.shadowRoot.getElementsByTagName('div')[0].style.top;
+                    }
             },
-            get: function(){
-                return this.shadowRoot.getElementsByTagName('div')[0].style.top;
-            }
-        },  "hidden": {
-            set: function (newVal) {
-                if (typeof newVal === 'boolean')
-                    this.hidden = newVal;
+            "hidden": {
+                set: function (newVal) {
+                    if (typeof newVal === 'boolean')
+                        this.hidden = newVal;
+                    },
+                get: function () {
+                    if (this.hidden)
+                        return this.hidden;
+                    }
             },
-            get: function () {
-                if (this.hidden)
-                    return this.hidden;
+            "table":  {
+                get: function(){
+                    return this.shadowRoot.getElementsByTagName('div')[0].getElementsByTagName('table')[0];
+                }
+            },
+            "rowcolor":{
+                set: function (newVal){
+                    this.rowcolor=newVal;
+                    },
+                get: function(){
+                    if(this.rowcolor)
+                    return this.rowcolor;
+                }
             }
-        }
         });
     PopMenuProto.createdCallback = function (){
         var shadow = this.createShadowRoot();
         var di = document.createElement('div');
         di.style.position = 'absolute';
         var tb = document.createElement('table');
-        tb.insertRow();
+        tb.id='PopMenuBar';
+        tb.cellSpacing=0;
         di.appendChild(tb);
         shadow.appendChild(di);
     };
     PopMenuProto.addItem = function(item){
-        var table= this.shadowRoot.getElementsByTagName('div')[0].getElementsByTagName('table')[0];
-        table.insertRow(table.rows.length);
+        this.table.insertRow(this.table.rows.length);
         var img = new Image();
         img.style.width = '10px';
         img.style.height = '10px';
         img.src = item.image;
-        if(img.src) table.rows[table.rows.length-1].insertCell(0).appendChild(img);
-        if(item.description)  table.rows[table.rows.length-1].insertCell(1).innerHTML = item.description;
-        if(item.hotkey) table.rows[table.rows.length-1].insertCell(2).innerHTML= item.hotkey;
+        if(img.src) this.table.rows[this.table.rows.length-1].insertCell(0).appendChild(img);
+        if(item.description)  this.table.rows[this.table.rows.length-1].insertCell(1).innerHTML = item.description;
+        if(item.hotkey) this.table.rows[this.table.rows.length-1].insertCell(2).innerHTML= item.hotkey;
+        this.table.rows[this.table.rows.length-1].cells[0].style.border='none';
+        this.table.rows[this.table.rows.length-1].cells[1].style.border='none';
+        this.table.rows[this.table.rows.length-1].cells[2].style.border='none';
     };
     PopMenuProto.addItems = function(items){
         for(var i = 0;i<items.count;i++){
@@ -212,16 +250,20 @@ var PopMenu = function(){
         }
     };
     PopMenuProto.MouseOverEvent = function(color){
-        var table = this.shadowRoot.getElementsByTagName('div')[0].getElementsByTagName('table')[0];
-        for(var i = 0; i<table.rows.length;i++){
-            table.rows[i].onmouseover = function(){
-                console.log(table.rows[i])
-                table.rows[i].style.backgroundColor= color;
-            }
+        var normal_color= this.row_color;
+        for(var i = 0; i<this.table.rows.length;i++){
+            this.table.rows[i].onmouseover = function(e){
+                this.style.cursor = "default";
+                this.style.backgroundColor= color;
+            };
+            this.table.rows[i].onmouseout = function(e){
+                this.style.backgroundColor = normal_color;
+            };
         }
     }
     PopMenuProto.setBackgroundColor= function(color){
          if(typeof color === 'string')  this.shadowRoot.getElementsByTagName('div')[0].style.backgroundColor=color;
+            this.row_color=color;
     };
     PopMenuProto.hide = function(){
         this.style.display = 'none';
@@ -232,12 +274,11 @@ var PopMenu = function(){
         this.hidden = false;
     };
     PopMenuProto.setFontSize = function(size){
-        this.shadowRoot.getElementsByTagName('div')[0].getElementsByTagName('table')[0].style.fontSize = size;
+        this.table.style.fontSize = size;
     };
     PopMenuProto.setFontSizeHotKey = function (size){
-        var table =  this.shadowRoot.getElementsByTagName('div')[0].getElementsByTagName('table')[0];
-        for(var i = 1; i<table.rows.length;i++){
-            table.rows[i].cells[2].style.fontSize = size;
+        for(var i = 0; i<this.table.rows.length;i++){
+            this.table.rows[i].cells[2].style.fontSize = size;
         }
     };
 
